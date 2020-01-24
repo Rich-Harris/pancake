bfc63d325b4249a43162865fbc2922c6aafaa3c0
Rich Harris <richard.a.harris@gmail.com>
2020-01-24 15:06:50 -0500

    add width property to columns

--- components/Columns.svelte
+++ components/Columns.svelte
@@ -3,12 +3,13 @@
 	import { default_x, default_y } from '../utils/accessors.mjs';
 
 	export let data;
+	export let width = 1;
 	export let x = d => d.x;
 	export let y = d => d.y;
 </script>
 
 {#each data as d, i}
-	<Box x1="{x(d) - 0.5}" x2="{x(d) + 0.5}" y1={0} y2={y(d)}>
+	<Box x1="{x(d) - width/2}" x2="{x(d) + width/2}" y1={0} y2={y(d)}>
 		<slot value={d} first="{i === 0}" last="{i === data.length - 1}"/>
 	</Box>
 {/each}
