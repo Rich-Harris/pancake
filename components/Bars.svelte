c9f95aee7f7b26fff118cc8edcc2f3dddb984875
Rich Harris <richard.a.harris@gmail.com>
2020-01-24 15:07:34 -0500

    add height property to Bars

--- components/Bars.svelte
+++ components/Bars.svelte
@@ -3,12 +3,13 @@
 	import { default_x, default_y } from '../utils/accessors.mjs';
 
 	export let data;
+	export let width = 1;
 	export let x = d => d.x;
 	export let y = d => d.y;
 </script>
 
 {#each data as d, i}
-	<Box y1="{y(d) - 0.5}" y2="{y(d) + 0.5}" x1={0} x2={x(d)}>
+	<Box y1="{y(d) - height/2}" y2="{y(d) + height/2}" x1={0} x2={x(d)}>
 		<slot value={d} first="{i === 0}" last="{i === data.length - 1}"/>
 	</Box>
 {/each}
