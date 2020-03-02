1a56f6679c76c23b63dc742052a7b4bccf96b085
Brian Cray <bcray@apple.com>
2020-03-02 09:12:47 -0500

    add plotGaps option to SvgLine and prevent plotting nulls by default

--- components/SvgLine.svelte
+++ components/SvgLine.svelte
@@ -7,10 +7,29 @@
 	export let data;
 	export let x = default_x;
 	export let y = default_y;
+    export let plotGaps = false
 
-	$: d = 'M' + data
-		.map(d => `${$x_scale(x(d))},${$y_scale(y(d))}`)
-		.join('L');
+    let d
+
+    $: if (plotGaps) {
+        d = 'M' + data
+            .map(d => `${$x_scale(x(d))},${$y_scale(y(d))}`)
+            .join('L')
+    }
+    else {
+        d = data
+            .map((d, i) => {
+                let motion = 'L'
+                if (y(d) == null) {
+                    return
+                }
+                if (i == 0 || y(data[i - 1]) == null) {
+                    motion = 'M'
+                }
+                return `${motion}${$x_scale(x(d))},${$y_scale(y(d))}`
+            })
+            .join('')
+    }
 </script>
 
 <slot {d}></slot>
